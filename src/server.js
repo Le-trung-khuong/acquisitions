0a7e9eafaf4a27a0daf2d8bbdcf275815900367b
khuong <letrungkhuong2006@gmail.com>
2026-02-28 15:32:03 +0700

    implement ESlint and prettier

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import app from './app.js';
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Acquisitions service is running on port http://localhost:${PORT}`);
+  console.log(
+    `Acquisitions service is running on port http://localhost:${PORT}`
+  );
 });
-
